f82bc88c7175dc411e6c0589f4e3e85371c91c5c
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-02-26 16:51:23 +0530

    first commit

---
+++ lecture10array/unique.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[9];
+    cout<<"Enter the elements in the array: "<<endl;
+    for(int i=0;i<9;i++){
+        cin>>arr[i];
+    } 
+    for(int i=0;i<9;i++){
+        for(int j=i+1;j<9;j++){
+            if(arr[i]==arr[j]){
+                arr[i]=arr[j]=-1;
+            }
+        }
+    }
+    for(int i=0;i<9;i++){
+        if(arr[i]>0){
+            cout<<"Unique element in the array is: "<<arr[i]<<endl;
+        }
+    }
+}
