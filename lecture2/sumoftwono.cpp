cd2fb15cdd2140db9bbfe2ebb1f2ee8f362f08a8
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-03-10 23:22:13 +0530

    first commit

---
+++ lecture2/sumoftwono.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s=0,a,b;
+    cout<<"Enter the value of two numbers: "<<endl;
+    cin>>a>>b;
+    s=a+b;
+    cout<<"Sum of two number is: "<<s<<endl;
+}
