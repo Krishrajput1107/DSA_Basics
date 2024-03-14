f32b0ac751bbab912d50d162cb1f9d266ef66f90
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-03-14 21:08:47 +0530

    first commit

--- lecture10array/secondlargest.cpp
+++ lecture10array/secondlargest.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int arr[5];
-    int max=INT16_MIN;
-    cout<<"Enter the elements in the array"<<endl;
-    for(int i=0;i<10;i++){
-        cin>>arr[i];
-    }
-    for(int i=0;i<10;i++){
-        if(arr[i]>max){
-            max=arr[i];
-        }
-    }
-    for(int i=0;i<10;i++){
-        if(arr[i]>max){
-            max=arr[i];
-        }
-    }
-    cout<<max;
 
+
+int largestelementIndex(int arr[],int size){
+    int i,max=INT16_MIN;
+    int maxindex=-1;
+    for(i=0;i<10;i++){
+        if(max<arr[i]){
+            max=arr[i];
+            maxindex=i;
+        }
+    }
+    return maxindex;
+}
+
+int main(){
+    int arr[]={2,3,1,4,7,5,6,9,8,0};
+    int max=INT16_MIN;
+    int indexoflargest=largestelementIndex(arr,10);
+    arr[indexoflargest]=-1;
+    int indexofsecondlargest=largestelementIndex(arr,10);
+    cout<<arr[indexofsecondlargest];
 }
